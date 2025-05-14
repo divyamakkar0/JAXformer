32f5d7052e4c3d176dbcac46325f6bbc770fe43a
divyamakkar0 <divyamakkar000@gmail.com>
2025-05-14 16:58:12 -0400

    finished? main

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from jax import random
 from flax import linen as nn
 from flax.training import train_state
 import time
+import math
+import optax
 
 from config import parse_args
 from model import Decoder
@@ -58,12 +60,23 @@ def train_step(loss_fn, params, key, *args, **kwargs):
     return grads, metrics
 
 def eval_step(loss_fn, params, key, *args, **kwargs):
-    loss, pred = loss_fn(params, key, *args, **kwargs, train=False)
+    loss, pred = loss_fn(params, key, *args, **kwargs, train=False)i t
     metrics = {
         'loss': loss,
         'pred': pred,
     }
     return metrics
+
+def learning_rate(time_step, warmup_steps, total_steps, min_rate, max_rate):
+    if time_step < warmup_steps:
+        return min_rate + (max_rate - min_rate) * (time_step/warmup_steps)
+    elif time_step <= total_steps:
+        decay_steps = total_steps - warmup_steps
+        decay_time = time_step - warmup_steps
+        cosine_decay = 0.5 * (1 + jnp.cos(math.pi * (decay_time / decay_steps)))
+        return min_rate + (max_rate - min_rate)*cosine_decay
+    else:
+        return min_rate
 
 def main(config):
    key = KeyState(config.seed) #fix this line
@@ -79,22 +92,39 @@ def main(config):
    total_steps = config.training_steps
    
    #cosine scheduler
+   lr_scheduler = optax.warmup_cosine_decay_schedule(
+       init_value=config.min_lr,
+       peak_value=config.max_lr,
+       warmup_steps=config.warmup_steps,
+       decay_steps=config.end_steps,
+       end_value=config.end_lr,
+    )
+
+
    #optax adam optimizer
+   tx = optax.adam(lr_scheduler)
+   state = train_state.TrainState.create(
+       apply_fn=model.apply,
+       params=params,
+       tx=tx,
+   )
 
    print("starting training")
+
    loss_fn = jax.tree_util.Partial(cross_entropy_loss, model)
    train_step_jit = jax.jit(
         lambda key, params, x, y : train_step(loss_fn, params, key, x, y)
     )
    eval_step_jit = jax.jit(
-        lambda key, params, x, y : eval_step( loss_fn, params, key, x, y)
+       lambda key, params, x, y : eval_step( loss_fn, params, key, x, y)
     )
    
    start = time.time()
    train_loss = 0.0
 
    for current_step in range(total_steps):
-        x_t, label_t, t, noise = get_batch(key=key(), scheduler=scheduler, data=train_dataset())
+        #get batch needs to be fixed
+        x_t, label_t = get_batch(key=key(), scheduler=scheduler, data=train_dataset())
         grads, metrics = train_step_jit(key(), state.params, x, label_t, t, noise)
         wandb.log({"step": current_step, "train_loss": metrics['loss']})
         state = state.apply_gradients(grads=grads)
