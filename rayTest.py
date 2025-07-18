27e7473826f5f4408128f07b2fdea538c9d77878
adityamakkar000 <aditymakkar000@gmail.com>
2025-07-18 11:34:34 -0400

    add tensor stuff

---
+++ rayTest.py
@@ -0,0 +1,10 @@
+import ray
+
+ray.init()
+
+print('''This cluster consists of
+    {} nodes in total
+    {} CPU resources in total
+'''.format(len(ray.nodes()), ray.cluster_resources()['CPU']))
+
+
