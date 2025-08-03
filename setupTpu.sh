a39bcdc24d4213c9bc925a7a13de2e9d4646bfaa
divyamakkar0 <divyamakkar000@gmail.com>
2025-08-03 01:13:16 +0000

    updated setup tpu

--- setupTpu.sh
+++ setupTpu.sh
@@ -4,6 +4,11 @@
 echo "installing dependencies ... "
 pip install -U "jax[tpu]"
 pip install flax jaxtyping wandb tpu-info einops tiktoken
+pip install google-cloud
+pip install google-cloud-storage
+pip install gcloud
+pip install gcloud-aio
+pip install gcloud-aio-storage
 
 echo "downloading fineweb 10B shards ... "
 bash fineweb.sh
