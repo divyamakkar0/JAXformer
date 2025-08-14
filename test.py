818896102da989c932b05f09f0fef919f21065a1
adityamakkar000 <aditymakkar000@gmail.com>
2025-08-14 11:18:04 -0400

    new push

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import jax
 import jax.numpy as jnp
+from functools import partial
+from jax.sharding import PartitionSpec as P
 import numpy as np
 
 print("initing ... ")
@@ -18,8 +20,17 @@ if jax.process_index() == 0:
 mesh = jax.make_mesh((32,), ('dp'))
 print(mesh)
 a = jnp.arange(32)
-a = jax.device_put(a, jax.NamedSharding(mesh, jax.sharding.PartitionSpec('dp')))
-b = jax.lax.all_gather(a, axis_name='dp', axis=0, tiled=True)
+a = jax.device_put(a, jax.NamedSharding(mesh, P('dp')))
+
+@partial(
+    jax.shard_map,
+    in_specs=(P('dp'),),
+    out_specs=P(),
+)
+def all_gather(x):
+    return jax.lax.all_gather(x, axis_name='dp', axis=0, tiled=True)
+
+b = all_gather(a)
 
 print(b)
 jax.debug.visualize_array_sharding(b)
