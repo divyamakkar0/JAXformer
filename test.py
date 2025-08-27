c25d98cf74c76f209dd3c5f27f179d848c3e9bcb
adityamakkar000 <aditymakkar000@gmail.com>
2025-08-27 14:28:43 -0400

    update pipeline

--- test.py
+++ test.py
@@ -16,7 +16,6 @@ jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 jax.config.update(
     "jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir"
 )
-# jax.config.update("jax_disable_jit", True)
 
 import optax
 from jax.sharding import PartitionSpec as P
