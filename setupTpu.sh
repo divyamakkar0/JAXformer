6c9daa5f531f4c7a04df793712694002b27afd08
adityamakkar000 <aditymakkar000@gmail.com>
2025-08-27 15:51:35 -0400

    update pipeline

--- setupTpu.sh
+++ setupTpu.sh
@@ -5,7 +5,7 @@ pip install -U "jax[tpu]"
 pip install flax jaxtyping wandb tpu-info einops tiktoken
 pip install google-cloud google-cloud-storage gcloud gcsfs
 
-if [[ -z .config/gcloud/application_default_credentials.json ]]; then
+if [[ -z "~/.config/gcloud/application_default_credentials.json" ]]; then
   echo "gcloud storage crendinatials found "
 fi
 
