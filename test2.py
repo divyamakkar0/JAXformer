f0e1eb78b4203c6128674f6aba4d153ddcaf3fee
adityamakkar000 <aditymakkar000@gmail.com>
2025-08-30 09:47:48 -0400

    update pipeline

--- test2.py
+++ test2.py
@@ -595,6 +595,7 @@ class Block(nn.Module):
         KR_cache = jnp.stack(KR_cache, axis=0) if len(KR_cache) > 0 else None
 
         out_cache = (KV_cache, KR_cache)
+        breakpoint()
 
         return x, (out_cache, moe_stat)
 
